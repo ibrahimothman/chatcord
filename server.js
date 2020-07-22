eaf934cbd94c6136544033a8771b371bd394f304
ibrahimothman <ibrahimothmanre@gmail.com>
2020-07-22 15:18:12 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = 3000 || process.env.PORT
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
